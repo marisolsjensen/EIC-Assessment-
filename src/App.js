26e84302dc14ec9ccd7ddc1492dbccc3eb10eacd
marisolsjensen <mje@ndeu.dk>
2025-02-12 16:59:57 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>EIC Accelerator Eligibility Assessment</h1>
+      <p>Welcome to the EIC Accelerator assessment tool.</p>
+    </div>
+  );
+}
